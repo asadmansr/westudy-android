b3922c2fedd5a55a48db614c2a6eb9ce3f42d9f9
Asad Mansoor <asad@poprx.ca>
2016-08-17 21:12:52 -0400

    initial commit

---
+++ app/src/main/java/com/projectg/westudy/LoginSignup/MainLoginActivity.java
@@ -0,0 +1,15 @@
+package com.projectg.westudy.LoginSignup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projectg.westudy.R;
+
+public class MainLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+    }
+}
