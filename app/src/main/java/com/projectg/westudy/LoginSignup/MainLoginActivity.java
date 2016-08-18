7dfb7c6b7f130e8b13b04441fe08d3ef4a4da316
asadmansr <asadmansr@gmail.com>
2016-08-18 16:31:28 -0400

    Created MainLoginActivity issue:4

--- app/src/main/java/com/projectg/westudy/LoginSignup/MainLoginActivity.java
+++ app/src/main/java/com/projectg/westudy/LoginSignup/MainLoginActivity.java
@@ -1,21 +1,52 @@
 package com.projectg.westudy.LoginSignup;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.projectg.westudy.R;
 
-public class MainLoginActivity extends AppCompatActivity {
-
-    //LoginActivity
+public class MainLoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private MainLoginViewHolder mMainLoginViewHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+        controlInitialization();
+    }
 
-        //Test code.
-        System.out.println("weStudy - the ultimate group study app.");
+    private void controlInitialization(){
+        mMainLoginViewHolder = new MainLoginViewHolder(findViewById(R.id.relative_container),this);
+    }
 
+    private class MainLoginViewHolder{
+        private LinearLayout mSignInLayout;
+        private RelativeLayout mJoinNowLayout;
+
+        private MainLoginViewHolder(View view, View.OnClickListener listener){
+            mSignInLayout = (LinearLayout) view.findViewById(R.id.sign_in_ll);
+            mJoinNowLayout = (RelativeLayout) view.findViewById(R.id.join_now_rl);
+
+            mSignInLayout.setOnClickListener(listener);
+            mJoinNowLayout.setOnClickListener(listener);
+        }
+    }
+
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.sign_in_ll:
+                Snackbar.make(view, "ToDo: sign in", Snackbar.LENGTH_SHORT).show();
+                break;
+
+            case R.id.join_now_rl:
+                Snackbar.make(view, "ToDo: join now", Snackbar.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
